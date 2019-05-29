5d14bf3861fc06733381384d1b84107d615c0607
brett <brett_matsumoto@yahoo.com>
2019-05-28 20:06:58 -1000

    added itemImages seed

--- server/database/seeds/itemImages.js
+++ server/database/seeds/itemImages.js
@@ -1,13 +1,22 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('itemImages').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('itemImages').insert([
+        {imageLink: 'test', item_id: 1},
+        {imageLink: 'test', item_id: 2},
+        {imageLink: 'test', item_id: 3},
+        {imageLink: 'test', item_id: 4},
+        {imageLink: 'test', item_id: 5},
+        {imageLink: 'test', item_id: 6},
+        {imageLink: 'test', item_id: 7},
+        {imageLink: 'test', item_id: 8},
+        {imageLink: 'test', item_id: 9},
+        {imageLink: 'test', item_id: 10},
+        {imageLink: 'test', item_id: 11},
+        {imageLink: 'test', item_id: 12},
       ]);
     });
 };
