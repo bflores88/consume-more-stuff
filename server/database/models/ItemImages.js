2d68ceed74524906d516f85b3e0ef5661c86b9bf
brett <brett_matsumoto@yahoo.com>
2019-05-29 11:57:06 -1000

    added itemImages model

---
+++ server/database/models/ItemImages.js
@@ -0,0 +1,18 @@
+const bookshelf = require('../bookshelf');
+
+require('./Item');
+
+class ItemImage extends bookshelf.Model {
+  get tableName() {
+    return 'itemImages';
+  }
+  get hasTimestamps() {
+    return true;
+  }
+
+  item_id() {
+    return this.belongsTo('Item', 'item_id');
+  }
+}
+
+module.exports = bookshelf.model('itemImage', ItemImage);
