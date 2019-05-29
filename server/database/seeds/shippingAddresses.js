5f8b271758b6feae54866116bf6c833a99ad9960
brett <brett_matsumoto@yahoo.com>
2019-05-28 18:21:15 -1000

    completed shipping address seeds

--- server/database/seeds/shippingAddresses.js
+++ server/database/seeds/shippingAddresses.js
@@ -1,13 +1,16 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('shippingAddresses').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('shippingAddresses').insert([
+        {street: '123 Easy Street', aptSuite: '', city: 'Honolulu', state: 'HI', country: 'USA', zip: '12345', user_id: '1'},
+        {street: '123 Sesame Street', aptSuite: '', city: 'Carson City', state: 'PA', country: 'USA', zip: '12345', user_id: '2'},
+        {street: '123 Kenya Street', aptSuite: 'Suite A', city: 'Gothem City', state: 'WA', country: 'USA', zip: '12345', user_id: '3'},
+        {street: '123 Malaysia Street', aptSuite: 'Apt 202', city: 'Marshmellow', state: 'NJ', country: 'USA', zip: '12345', user_id: '4'},
+        {street: '123 Dakka Dakka Lane', aptSuite: '', city: 'Milwalkee', state: 'TX', country: 'USA', zip: '12345', user_id: '5'},
+        {street: '123 Durka Dur Street', aptSuite: '', city: 'Canton', state: 'VA', country: 'USA', zip: '12345', user_id: '6'},
       ]);
     });
 };
