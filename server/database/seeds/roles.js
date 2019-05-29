0ccdb32a6ccd1f98a5815cc125e8df7df5578678
brett <brett_matsumoto@yahoo.com>
2019-05-28 16:37:37 -1000

    created roles seed

---
+++ server/database/seeds/roles.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('roles').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('roles').insert([
+        {roleName: 'Admin', canBuy: true, canSell: true, canModify: true, canApprove: true, canSuspend: true},
+        {roleName: 'Moderator', canBuy:true, canSell: true, canModify: true, canApprove: true, canSuspend: false},
+        {roleName: 'User', canBuy: true, canSell: true, canModify: false, canApprove: false, canSuspend: false},
+        {roleName: 'Guest', canBuy: false, canSell: false, canModify: false, canApprove: false, canSuspend: false}
+      ]);
+    });
+};
