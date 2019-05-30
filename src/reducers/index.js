1213647e01ff1e80f74a96f8750082f06c13ea7c
brett <brett_matsumoto@yahoo.com>
2019-05-30 10:41:32 -1000

    added LOAD_SPECIFIC_ITEM reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 'use strict';
 import { LOAD_ITEMS } from '../actions';
+import { LOAD_SPECIFIC_ITEM } from '../actions';
 
 const initialState = {
+  item: {},
   items: [],
 };
 
@@ -9,6 +11,8 @@ function itemReducer(state = initialState, action) {
   switch (action.type) {
     case LOAD_ITEMS:
       return Object.assign({}, state, { items: [...action.payload] });
+    case LOAD_SPECIFIC_ITEM:
+      return Object.assign({}, state, { item: action.payload} );
     default:
       return state;
   }
