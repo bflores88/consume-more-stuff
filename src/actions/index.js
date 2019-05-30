2a7623eb8e2133c7c0066ec229d836fc1ea0cf8b
Frank <fr.heggeness@gmail.com>
2019-05-29 19:54:58 -1000

    small kine broke

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@
 // ACTION DEFINTION
 
 export const LOAD_ITEMS = 'LOAD_ITEMS';
-export const GRAB_ITEM_IMAGE = 'GRAB_ITEM_IMAGE';
+export const GRAB_ITEM_IMAGES = 'GRAB_ITEM_IMAGE';
 
 // ACTION CREATOR
 
@@ -17,6 +17,23 @@ export const loadItems = () => {
         console.log(items);
         return dispatch({
           type: LOAD_ITEMS,
+          payload: items,
+        });
+      })
+      .catch((err) => console.log('Cant access website' + err));
+  };
+};
+
+export const grabItemImages = () => {
+  return (dispatch) => {
+    return fetch(`/api/images`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((items) => {
+        console.log(items);
+        return dispatch({
+          type: GRAB_ITEM_IMAGES,
           payload: items,
         });
       })
