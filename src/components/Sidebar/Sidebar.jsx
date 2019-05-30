cbe1275df3b3f3727c83030606b7a3bc969a1c83
kmietotwy <kmietotwy@gmail.com>
2019-05-29 14:34:31 -1000

    sidebar with temp props

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,8 @@ import './Sidebar.scss';
 
 const Sidebar = (props) => {
 
-  //need props for currentUser and role
+  //need props for isLoggedIn and role
+  const { isLoggedIn, role } = props;
 
   //generate links for items by category
   const createItemCategoryLinks = () => {
@@ -25,7 +26,115 @@ const Sidebar = (props) => {
     </div>
   };
 
-  //conditional statement to show if currentUser is true or false (logged in or note) AND check the role
+  //conditional statement to show if isLoggedIn is true or false (logged in or not) AND check the role
+
+  //first if statement is for non-users
+  //second if statement is for admin and mods
+  //else is for users
+
+  if (!isLoggedIn) {
+    return (
+      <div className="public-nav">
+        <div className="logo">
+          {/* insert logo here */}
+          <Link to="/">
+            SAVANNAH
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/home">
+            <button>Home</button>
+          </Link>
+        </div>
+
+        <div className="cat-links">
+          {createItemCategoryLinks()}
+        </div>
+      </div>
+    );
+  } else if (isLoggedIn && role !== "user") {
+    return (
+      <div className="admin-nav">
+        <div className="logo">
+          {/* insert logo here */}
+          <Link to="/">
+            SAVANNAH
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/admin/items">
+            Items
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/admin/categories">
+            Categories
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/admin/settings">
+            Settings
+          </Link>
+        </div>
+
+        <div className="cat-links">
+          {createItemCategoryLinks()}
+        </div>
+
+      </div>
+    );
+  } else {
+    return (
+      <div className="user-nav">
+        <div className="logo">
+          {/* insert logo here */}
+          <Link to="/">
+            SAVANNAH
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/home">
+            <button>Home</button>
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/users/messages">
+            <button>Messages</button>
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/users/settings">
+            <button>Settings</button>
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/users/items/new">
+            <button>Post Items</button>
+          </Link>
+        </div>
+
+        <div className="nav-links">
+          <Link to="/users/items">
+            <button>Edit Items</button>
+          </Link>
+        </div>
+
+        <div className="cat-links">
+          {createItemCategoryLinks()}
+        </div>
+
+      </div>
+    )
+  }
+
 }
 
 export default Sidebar;
