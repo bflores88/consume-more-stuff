04735a21b4e622fcfcc7556bbc44abf3d455eb9c
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-05-31 11:04:53 -1000

    get user, put profile, theme, password

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,17 +3,87 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../database/models/User');
+const bcrypt = require('bcryptjs');
+const saltRounds = 12;
 
 router.route('/').get((req, res) => {
-  new User()
-    .fetchAll()
+  new User({ id: req.user.id })
+    .fetch()
     .then((result) => {
-      // respond with all users
+      // reply with logged in user
       return res.json(result);
     })
     .catch((err) => {
-      return res.status(500).send('Server error');
+      console.log('error:', err);
     });
+});
+
+// edit general profile
+router.route('/profile').put((req, res) => {
+  new User('id', req.user.id)
+    .save({
+      name: req.body.name,
+      email: req.body.email,
+    })
+    .then((result) => {
+      new User({ id: req.user.id })
+        .fetch()
+        .then((result) => {
+          const user = result.toJSON();
+          return res.send(user);
+        })
+        .catch((err) => {
+          console.log('error:', err);
+        });
+    });
+});
+
+// change theme
+router.route('/theme').put((req, res) => {
+  new User('id', req.user.id)
+    .save({
+      theme_id: req.body.theme_id,
+    })
+    .then((result) => {
+      new User({ id: req.user.id })
+        .fetch()
+        .then((result) => {
+          const user = result.toJSON();
+          return res.send(user);
+        })
+        .catch((err) => {
+          console.log('error:', err);
+        });
+    });
+});
+
+// change password
+router.route('/password').put((req, res) => {
+  bcrypt.genSalt(saltRounds, (err, salt) => {
+    if (err) {
+      console.log('error:', err);
+      return res.status(500).send('Unable to generate salt');
+    }
+
+    bcrypt.hash(req.body.password, salt, (err, hash) => {
+      if (err) {
+        console.log('error:', err);
+        return res.status(500).send('Unable to encrypt');
+      }
+
+      new User('id', req.user.id)
+        .save({ password: hash })
+        .then((result) => {
+          new User({ id: req.user.id }).fetch().then((result) => {
+            return res.json(result);
+          });
+        })
+        .catch((err) => {
+          console.log('error:', err);
+          return res.status(500).send('Error changing password');
+        });
+    });
+  });
 });
 
 module.exports = router;
