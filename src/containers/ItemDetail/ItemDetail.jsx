0605465f4f531e8191cdef6d8b57cb30b3d4f48b
kmietotwy <kmietotwy@gmail.com>
2019-05-31 12:00:49 -1000

    /items/:id now changes

--- src/containers/ItemDetail/ItemDetail.jsx
+++ src/containers/ItemDetail/ItemDetail.jsx
@@ -2,67 +2,50 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { loadSpecificItem } from '../../actions';
 
-const ItemDetail = (props) => {
 
-  console.log(props);
+class ItemDetail extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+    };
+  }
 
-  console.log(props.match.params);
+  componentDidMount() {
+    return this.props.loadSpecificItem(this.props.match.params.id)
+  }
 
-  const { category } = props.match.params;
+  componentDidUpdate(prevProps) {
+    if (this.props.match.params.id !== prevProps.match.params.id) {
+      return this.props.loadSpecificItem(this.props.match.params.id)
+    }
+  }
 
-  console.log(category);
+  render() {
+    console.log('12312312312312',this.props.item)
+    return <div>
+    {this.props.item.name}
+      {this.props.item.description}
+      <h1>Item Detail Page</h1>
+    </div>;
+  }
+}
 
-  return <div>Item Detail Page</div>;
-
-
+const mapStateToProps = (state) => {
+  return {
+    item: state.itemReducer.item,
+  };
 };
 
-// class ItemDetail extends Component {
-//   constructor(props) {
-//     super(props);
-    
-//     this.state = {
-//       name: ''
-//     };
-//   }
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadSpecificItem: (item) => dispatch(loadSpecificItem(item)),
+  };
+};
 
-//   componentDidMount() {
-//     console.log(this.props)
-//     return this.props.loadSpecificItem(6)
-//     // .then((response) => {
-//     //   return this.setState({
-//     //     name: response.payload.name,
-//     //     description: response.payload.description,
-
-//     //   })
-//     // });
-//   }
-
-//   render() {
-//     console.log('12312312312312',this.props.item)
-//     return <div>
-//     {this.props.item.name}
-//       {this.props.item.description}
-//       <h1>Item Detail Page</h1>
-//     </div>;
-//   }
-// }
-
-// const mapStateToProps = (state) => {
-//   return {
-//     item: state.itemReducer.item,
-//   };
-// };
-
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     loadSpecificItem: (item) => dispatch(loadSpecificItem(item)),
-//   };
-// };
-
-// ItemDetail = connect(
-//   mapStateToProps,
-//   mapDispatchToProps,
-// )(ItemDetail);
+ItemDetail = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(ItemDetail);
 
 export default ItemDetail;
