5d6d7c1898b3a2e125ccd5ce0ae38c040fa6e26a
kmietotwy <kmietotwy@gmail.com>
2019-05-31 13:50:22 -1000

    items on home go to item detail on click

--- src/containers/Item/Item.jsx
+++ src/containers/Item/Item.jsx
@@ -8,57 +8,54 @@ class Item extends Component {
   constructor(props) {
     super(props);
     this.state = {};
-
   }
 
   render() {
     const { name, id, price, imageLink } = this.props;
     const itemID = id;
     const redirectTo = `/items/${itemID}`;
-    // console.log(this.props.id);
 
     if (imageLink) {
       return (
-
-        <div className="singleItem">
-          <div className="imageBox">
-            <img
-              // src="https://3dexport.com/items/2018/07/11/530458/205933/rigged_cartoon_giraffe_model_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_2172968_o.jpg"
-              src={imageLink}
-              alt=""
-              className="itemImage"
-            />
+        <Link to={redirectTo}>
+          <div className="singleItem">
+            <div className="imageBox">
+              <img
+                // src="https://3dexport.com/items/2018/07/11/530458/205933/rigged_cartoon_giraffe_model_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_2172968_o.jpg"
+                src={imageLink}
+                alt=""
+                className="itemImage"
+              />
+            </div>
+            <div className="itemDescriptionBox">
+              <h4 className="itemName">{name}</h4>
+              <div className="itemPrice">Price: {price}</div>
+            </div>
+            <div />
           </div>
-          <div className="itemDescriptionBox">
-            <h4 className="itemName">{name}</h4>
-            {/* <div>id={id}</div> */}
-            <div className="itemPrice">Price: {price}</div>
-          </div>
-          <div />
-
-        </div>
+        </Link>
       );
     } else {
       return (
-        <div className="singleItem">
-          <div className="imageBox">
-            {/* <img
+        <Link to={redirectTo}>
+          <div className="singleItem">
+            <div className="imageBox">
+              {/* <img
               // src="https://3dexport.com/items/2018/07/11/530458/205933/rigged_cartoon_giraffe_model_3d_model_c4d_max_obj_fbx_ma_lwo_3ds_3dm_stl_2172968_o.jpg"
               src={imageLink}
               alt=""
               className="itemImage"
             /> */}
+            </div>
+            <div className="itemDescriptionBox">
+              <h4 className="itemName">{name}</h4>
+              <div className="itemPrice">Price: {price}</div>
+            </div>
+            <div />
           </div>
-          <div className="itemDescriptionBox">
-            <h4 className="itemName">{name}</h4>
-            {/* <div>id={id}</div> */}
-            <div className="itemPrice">Price: {price}</div>
-          </div>
-          <div />
-        </div>
+        </Link>
       );
     }
-
   }
 }
 
