30221e0ac47607bccf57b1b2fd310c4fa224c541
kmietotwy <kmietotwy@gmail.com>
2019-05-31 11:52:20 -1000

    removed items from Home bc it is not used

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,7 +4,6 @@ import { connect } from 'react-redux';
 import { loadItems } from '../../actions';
 
 import ItemsBox from '../../containers/ItemsBox';
-import Item from '../../containers/Item';
 
 class Home extends React.Component {
   constructor(props) {
