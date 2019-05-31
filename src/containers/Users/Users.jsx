48476dd0a4a19aa2c2a06cdc5a8adc6d5b984b93
kmietotwy <kmietotwy@gmail.com>
2019-05-31 11:30:11 -1000

    removed unnecessary console logs

--- src/containers/Users/Users.jsx
+++ src/containers/Users/Users.jsx
@@ -11,21 +11,16 @@ class Users extends Component {
   }
 
   componentDidMount() {
-    // console.log(this.props.currentUser.id)
     const user = this.props.match.params.id;
-    console.log(user);
   }
 
   componentDidUpdate(prevProps) {
     if (this.props.match.params.id !== prevProps.match.params.id) {
       const user = this.props.match.params.id;
-
-      console.log(user);
     }
   }
 
   render() {
- 
     if (!this.props.currentUser) {
       return <Redirect to="/" />;
     } else if (this.props.currentUser.role_id === 3 && this.props.match.params.id === "all") {
