8fb95955a2a74ff0f222eee8d91987bbb7113b9c
kmietotwy <kmietotwy@gmail.com>
2019-05-31 18:45:52 -1000

    basic user profile completed

--- src/containers/Users/Users.jsx
+++ src/containers/Users/Users.jsx
@@ -24,15 +24,52 @@ class Users extends Component {
   }
 
   render() {
-    console.log(this.props.user)
+  
     if (!this.props.currentUser) {
       return <Redirect to="/" />;
-    } else if (this.props.currentUser.role_id === 3 && this.props.match.params.id === "all") {
+    } else if (this.props.currentUser.role_id === 3 && this.props.match.params.id === 'all') {
       return <Redirect to="/" />;
-    } else if (this.props.match.params.id !== "all") {
-      return <h1>Single Users Page</h1>
-    }else {
-      return <h1>Users Page</h1>;
+    } else if (this.props.match.params.id !== 'all') {
+      if (!this.props.user.username) {
+        return <><div>Page Loading...</div></>;
+      } else {
+        console.log(this.props.user);
+        const user = {
+          username: this.props.user.username,
+          name: this.props.user.name,
+          email: this.props.user.email,
+          role: this.props.user.roles.roleName,
+          image: this.props.user.profileImageUrl,
+          active: this.props.user.active
+        };
+
+        let status;
+        if (user.active) {
+          status="ACTIVE"
+        } else {
+          status="INACTIVE"
+        }
+
+        return (
+          <div className="user-profile">
+            <div className="user-img">
+              <img className="prof-img" src={user.image} alt="user-image" />
+            </div>
+
+            <div className="user-details">
+              <h2>Username:&nbsp;{user.username}</h2>
+
+              <p className="user-detail">Name:&nbsp;{user.name}</p>
+              <p className="user-detail">Email:&nbsp;{user.email}</p>
+              <p className="user-detail">Role:&nbsp;{user.role}</p>
+              <p className="user-detail">Status:&nbsp;{status}</p>
+            </div>
+
+          </div>
+        )
+      }
+    } else {
+      return (<h1>Users Page</h1>);
     }
   }
 }
@@ -40,7 +77,7 @@ class Users extends Component {
 const mapStateToProps = (state) => {
   return {
     currentUser: state.itemReducer.currentUser,
-    user: state.itemReducer.user
+    user: state.itemReducer.user,
   };
 };
 
