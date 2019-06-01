fc001728b2851be3817bf94062db7d802384fb1b
kmietotwy <kmietotwy@gmail.com>
2019-05-31 18:56:02 -1000

    removed console log

--- src/containers/Users/Users.jsx
+++ src/containers/Users/Users.jsx
@@ -24,30 +24,32 @@ class Users extends Component {
   }
 
   render() {
-  
     if (!this.props.currentUser) {
       return <Redirect to="/" />;
     } else if (this.props.currentUser.role_id === 3 && this.props.match.params.id === 'all') {
       return <Redirect to="/" />;
     } else if (this.props.match.params.id !== 'all') {
       if (!this.props.user.username) {
-        return <><div>Page Loading...</div></>;
+        return (
+          <>
+            <div>Page Loading...</div>
+          </>
+        );
       } else {
-        console.log(this.props.user);
         const user = {
           username: this.props.user.username,
           name: this.props.user.name,
           email: this.props.user.email,
           role: this.props.user.roles.roleName,
           image: this.props.user.profileImageUrl,
-          active: this.props.user.active
+          active: this.props.user.active,
         };
 
         let status;
         if (user.active) {
-          status="ACTIVE"
+          status = 'ACTIVE';
         } else {
-          status="INACTIVE"
+          status = 'INACTIVE';
         }
 
         return (
@@ -63,13 +65,20 @@ class Users extends Component {
               <p className="user-detail">Email:&nbsp;{user.email}</p>
               <p className="user-detail">Role:&nbsp;{user.role}</p>
               <p className="user-detail">Status:&nbsp;{status}</p>
+              <br />
+              <div className="profile-button">
+                <button>Edit Profile</button>
+              </div>
+              <br />
+              <div className="profile-button">
+                <button>Deactivate My Account</button>
+              </div>
             </div>
-
           </div>
-        )
+        );
       }
     } else {
-      return (<h1>Users Page</h1>);
+      return <h1>Users Page</h1>;
     }
   }
 }
