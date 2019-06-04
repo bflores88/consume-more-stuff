88c25d7fef3cebb8e94978f18c0d8008e4e38e6c
brett <brett_matsumoto@yahoo.com>
2019-06-04 10:37:20 -1000

    removed console.log

--- server/middleware/isLoggedInGuard.js
+++ server/middleware/isLoggedInGuard.js
@@ -4,7 +4,6 @@ module.exports = function(req, res, next) {
   if (req.isAuthenticated()) {
     return next();
   } else {
-    console.log('User Guard Blocked Me');
     return res.send('You are not Authorized to view this page.');
   }
 };
