40155f94bf528aabc4cb2ee03f49c402c06dc2d5
brett <brett_matsumoto@yahoo.com>
2019-06-05 11:23:29 -1000

    required the correct model and adjusted .where to be id: req.params.id

--- server/middleware/shippingAddressGuard.js
+++ server/middleware/shippingAddressGuard.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const ShippingAddress = require('../database/models/shipping_addresses');
+const ShippingAddress = require('../database/models/shippingAddress');
 
 module.exports = function(req, res, next) {
-  ShippingAddress.where({ user_id: req.user.id })
+  ShippingAddress.where({ id: req.params.id })
     .fetch({ withRelated: ['users'] })
     .then((result) => {
       const shipAddressUserId = result.toJSON().user_id;
