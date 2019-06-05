e97f7109087b29d00fdf82e58919e4912107730c
brett <brett_matsumoto@yahoo.com>
2019-06-05 10:38:39 -1000

    created guard for payment cards

---
+++ server/middleware/paymentCardGuard.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const PaymentCard = require('../database/models/PaymentCard');
+
+module.export = function(req, res, next) {
+  PaymentCard.where({ user_id: req.user.id })
+    .fetch({ withRelated: ['users'] })
+    .then((result) => {
+      const cardUserId = result.toJSON().user_id;
+      if (cardUserId === req.user.id) {
+        return next();
+      } else {
+        return res.send('You are not Authorized to view this.');
+      }
+    });
+};
