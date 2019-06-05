a79276362fd909f0f2c2f22502144c3de25399fd
kmietotwy <kmietotwy@gmail.com>
2019-06-04 18:41:43 -1000

    basic set up folders

---
+++ src/containers/AdminCategories/AdminCategories.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './AdminCategories.scss';
+
+class AdminCategories extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <h1> All Categories </h1>
+    )
+  }
+}
+
+export default AdminCategories;
