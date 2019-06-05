1e7e0c66db698400e63deab6a00d96b630deaeda
brett <brett_matsumoto@yahoo.com>
2019-06-05 09:39:53 -1000

    renamed ownershipGuard to itemOwnerGuard

---
+++ server/middleware/itemOwnerGuard.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const Item = require('../database/models/Item');
+
+module.exports = function(req, res, next) {
+  Item.where({ id: req.params.id })
+    .fetch({ withRelated: ['users'] })
+    .then((result) => {
+      console.log(result)
+      const itemUserId = result.toJSON().user_id
+      if (itemUserId === req.user.id || req.user.role_id === 1) {
+        return next()
+      } else {
+        return res.send('You are not Authorized to view this page.')
+      }
+    })
+};
