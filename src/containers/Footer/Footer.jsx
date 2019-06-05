fe89da8e5d011bbbc7f4ddab40eee32b712e1b09
kmietotwy <kmietotwy@gmail.com>
2019-06-04 14:02:10 -1000

    renamed class to remove duplicate prop error

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -12,7 +12,7 @@ class Footer extends Component {
       <div className="footer">
         <div className="logoBox">
           <h4 className="title">
-            <i className="sunLogo" className="fas fa-sun" />
+            <i className="sunLogo-footer" className="fas fa-sun" />
             Savannah
           </h4>
         </div>
