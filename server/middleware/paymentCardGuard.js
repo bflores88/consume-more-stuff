916c68387e510a6048f1ca3ed3e0e90d6c77d4bc
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-06-05 15:12:36 -1000

    fixed module.exports reference

--- server/middleware/paymentCardGuard.js
+++ server/middleware/paymentCardGuard.js
@@ -2,12 +2,12 @@
 
 const PaymentCard = require('../database/models/PaymentCard');
 
-module.export = function(req, res, next) {
+module.exports = function(req, res, next) {
   PaymentCard.where({ id: req.params.id })
     .fetch()
     .then((result) => {
       const cardUserId = result.toJSON().user_id;
-      if (cardUserId === req.user.id) {
+      if (cardUserId === parseInt(req.user.id)) {
         return next();
       } else {
         return res.send('You are not Authorized to view this.');
