aa9f9ce9ba37c7406ce8cba19f8b7788451fa461
brett <brett_matsumoto@yahoo.com>
2019-06-06 13:24:32 -1000

    files made from npm run build

---
+++ public/precache-manifest.3dda8a8b40aa79b4194532c3f4e7beda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3d0b3c59b84ede5e634186765e016a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4526ca386dcf219b9369",
+    "url": "/static/css/main.1aea8ff0.chunk.css"
+  },
+  {
+    "revision": "8bd060e084dd9dbe63b0",
+    "url": "/static/js/2.03f6acb2.chunk.js"
+  },
+  {
+    "revision": "4526ca386dcf219b9369",
+    "url": "/static/js/main.8b1800bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
