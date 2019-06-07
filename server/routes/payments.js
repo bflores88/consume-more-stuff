109dc59eb6382cb47c176cc122b7ffc782947a45
Luke Fiorio <lukefiorio@Lukes-MBP.home>
2019-06-07 12:34:15 -1000

    incorporate active and add delete route

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -9,12 +9,13 @@ const paymentCardGuard = require('../middleware/paymentCardGuard');
 router
   .route('/')
   .get(isLoggedInGuard, (req, res) => {
-    PaymentCard.where({ user_id: req.user.id })
+    PaymentCard.where({ user_id: req.user.id, active: true })
+      .orderBy('active', 'DESC')
       .orderBy('primary', 'DESC')
       .orderBy('id', 'ASC')
       .fetchAll({ withRelated: ['states'] })
       .then((result) => {
-        // respond with all payment cards, sorted
+        // respond with all active payment cards, sorted
         return res.json(result);
       })
       .catch((err) => {
@@ -23,11 +24,11 @@ router
       });
   })
   .post(isLoggedInGuard, (req, res) => {
-    // get all of user's cards
-    PaymentCard.where({ user_id: req.user.id })
+    // get all of user's active cards
+    PaymentCard.where({ user_id: req.user.id, active: true })
       .fetchAll()
       .then((result) => {
-        // if no cards, set primary = true. Otherwise, false
+        // if no active cards, set primary = true. Otherwise, false
         const primary = result.length > 0 ? false : true;
         // return posted card (next: get all of user's cards)
         return new PaymentCard().save({
@@ -47,8 +48,56 @@ router
         });
       })
       .then(() => {
-        // return all of user's cards, sorted (next: send response)
-        return PaymentCard.where({ user_id: req.user.id })
+        // return all of user's active cards, sorted (next: send response)
+        return PaymentCard.where({ user_id: req.user.id, active: true })
+          .orderBy('active', 'DESC')
+          .orderBy('primary', 'DESC')
+          .orderBy('id', 'ASC')
+          .fetchAll({ withRelated: ['states'] });
+      })
+      .then((result) => {
+        // respond with all active payment cards
+        return res.json(result);
+      })
+      .catch((err) => {
+        console.log('error:', err);
+        return res.status(500).send('Server error');
+      });
+  });
+
+router
+  .route('/:id')
+  .put(isLoggedInGuard, paymentCardGuard, (req, res) => {
+    // get all of user's cards
+    PaymentCard.where({ user_id: req.user.id, active: true })
+      .fetchAll()
+      .then((result) => {
+        if (result.length < 1) {
+          throw new Error('Must have at least one active credit card to toggle primary.');
+        }
+        // return primary credit card (next: update 'primary' to false)
+        return PaymentCard.where({ user_id: req.user.id, primary: true }).fetch();
+      })
+      .then((result) => {
+        // if trying to set primary card to primary: exit out.
+        if (result.id === parseInt(req.params.id)) {
+          throw new Error(`This is already the user's primary card`);
+        }
+        // return prior primary card to false (next: update requested 'primary' to true)
+        return new PaymentCard('id', result.id).save({
+          primary: false,
+        });
+      })
+      .then(() => {
+        // return requested card as primrary (next: get all of user's cards)
+        return new PaymentCard('id', parseInt(req.params.id)).save({
+          primary: true,
+        });
+      })
+      .then(() => {
+        // return all of user's active cards, sorted (next: send response)
+        return PaymentCard.where({ user_id: req.user.id, active: true })
+          .orderBy('active', 'DESC')
           .orderBy('primary', 'DESC')
           .orderBy('id', 'ASC')
           .fetchAll({ withRelated: ['states'] });
@@ -58,53 +107,45 @@ router
         return res.json(result);
       })
       .catch((err) => {
-        console.log('error:', err);
+        console.log(err.message);
+        return res.status(500).send('Server error');
+      });
+  })
+  .delete(isLoggedInGuard, paymentCardGuard, (req, res) => {
+    // get all of user's active cards
+    PaymentCard.where({ user_id: req.user.id, active: true })
+      .fetchAll()
+      .then((result) => {
+        const cards = result.toJSON();
+        const cardToDelete = cards.find((card) => card.id === parseInt(req.params.id));
+        const cardToPrimary = cards.find((card) => card.id !== parseInt(req.params.id));
+
+        // if card-to-delete is the only active card or is not the primary card...
+        if (result.length === 1 || !cardToDelete.primary) {
+          // defer (next: set orig card to inactive & nonprimary)
+          return;
+          // if multiple active cards & trying to delete primary...
+        } else {
+          // return conversion of active secondary card to primary (next: set orig card to inactive/nonprimary)
+          return new PaymentCard('id', cardToPrimary.id).save({
+            primary: true,
+          });
+        }
+      })
+      .then(() => {
+        // return conversion of orig card to inactive/secondary (next: send response)
+        return new PaymentCard('id', parseInt(req.params.id)).save({
+          primary: false,
+          active: false,
+        });
+      })
+      .then(() => {
+        return res.send('Successful delete');
+      })
+      .catch((err) => {
+        console.log('error', err);
         return res.status(500).send('Server error');
       });
   });
-
-router.route('/:id').put(isLoggedInGuard, paymentCardGuard, (req, res) => {
-  // get all of user's cards
-  PaymentCard.where({ user_id: req.user.id })
-    .fetchAll()
-    .then((result) => {
-      if (result.length < 2) {
-        throw new Error('Must have at least two credit cards to change primary card.');
-      }
-      // return primary credit card (next: update 'primary' to false)
-      return PaymentCard.where({ user_id: req.user.id, primary: true }).fetch();
-    })
-    .then((result) => {
-      // if trying to set primary card to primary: exit out.
-      if (result.id === parseInt(req.params.id)) {
-        throw new Error(`This is already the user's primary card`);
-      }
-      // return prior primary card to false (next: update requested 'primary' to true)
-      return new PaymentCard('id', result.id).save({
-        primary: false,
-      });
-    })
-    .then(() => {
-      // return requested card as primrary (next: get all of user's cards)
-      return new PaymentCard('id', parseInt(req.params.id)).save({
-        primary: true,
-      });
-    })
-    .then(() => {
-      // return all of user's cards, sorted (next: send response)
-      return PaymentCard.where({ user_id: req.user.id })
-        .orderBy('primary', 'DESC')
-        .orderBy('id', 'ASC')
-        .fetchAll({ withRelated: ['states'] });
-    })
-    .then((result) => {
-      // respond with all payment cards
-      return res.json(result);
-    })
-    .catch((err) => {
-      console.log(err.message);
-      return res.status(500).send('Server error');
-    });
-});
 
 module.exports = router;
